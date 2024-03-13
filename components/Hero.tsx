ec1eba37f17508699ce2a7ef5c3e09d1e433f60f
xBalbinus <xiangan9@gmail.com>
2024-03-12 20:21:34 -0400

    fix: 1440: background / about

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,18 +6,18 @@ import AHSVG from './svgs/AH';
 
 export default function Hero() {
   return (
-    <div className={styles.heroContainer}>
-      <PageGutterWrapper>
+    <PageGutterWrapper>
+      <div className={styles.heroContainer}>
         <div className={styles.heroSection}>
           <div className={styles.mainHeroTitle}>
             <span className={styles.title}>Aliaksandr Hudzilin</span>
           </div>
           <div className={styles.content}>
-            <img src="/media/hudzilin.png" alt="Aliaksandr Hudzilin" style={{width: "100%", height: "100%"}} />
-            <div className={styles.textContainer}>Fueling the next wave of visionary entrepreneurs.</div>
+            <img src="/media/hudzilin.png" alt="Aliaksandr Hudzilin" style={{ width: '100%', height: '100%' }} />
+            <p className={styles.textContainer}>Fueling the next wave of visionary entrepreneurs.</p>
           </div>
         </div>
-      </PageGutterWrapper>
-    </div>
+      </div>
+    </PageGutterWrapper>
   );
 }
