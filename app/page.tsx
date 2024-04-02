a7a74615f833d1c21a6ede070828dc45450decd8
xBalbinus <xiangan9@gmail.com>
2024-04-02 09:03:52 -0400

    fixes: upload // caching

--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,7 @@ async function fetchDataFromAPI() {
     headers: {
       'X-API-KEY': api_key,
       'Content-Type': 'application/json',
-      'Cache-Control': 'no-cache',
+      'Cache-Control': 'no-store',
     },
     body: JSON.stringify({ site }),
   });
@@ -86,6 +86,7 @@ export default async function Page(props) {
     if (response && response.data) {
       data = { companies: response.data };
     }
+    data.companies.forEach(company => console.log(company));
   } catch (e) {
     console.error('Failed to fetch list data:', e.message);
   }
--- components/Company.tsx
+++ components/Company.tsx
@@ -4,18 +4,19 @@ import Link from './Link';
 import { FadeInSection } from '@components/FadeInSection';
 
 export default function Company({ companyLink, companyName, companyLogo, backgroundImage }) {
-  const companyURL = `https://${companyLink}`;
+  const companyURL = companyLink.startsWith('https://') ? companyLink : `https://${companyLink}`;
+  const displayLink = companyLink.replace(/^https:\/\/|\/$/g, '');
 
   return (
-    <Link href={companyURL} target="_blank">
+    <Link href={companyURL} target="_blank" key={companyName}>
       <FadeInSection>
         <div className={styles.boxContainer}>
           <div className={styles.backgroundImageContainer}>
             <img src={backgroundImage} alt="styling gradient" className={styles.backgroundImage} />
           </div>
           <div className={styles.box}>
-            <img src={companyLogo} alt="company logo" className={styles.logo} />
-            <div className={styles.boxLink}>{companyLink}</div>
+            <img src={companyLogo} alt={`${companyName} logo`} className={styles.logo} />
+            <div className={styles.boxLink}>{displayLink}</div>
           </div>
         </div>
       </FadeInSection>
